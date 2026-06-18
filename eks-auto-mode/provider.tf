f09f7903a455e47f8036f4ff0465df5a4dd7e07f
sms-bamboo <sunbirdhot@gmail.com>
2026-06-18 12:02:53 +0000

    feat: kubectl provider 설정 변경

--- eks-auto-mode/provider.tf
+++ eks-auto-mode/provider.tf
@@ -58,5 +58,4 @@ provider "kubectl" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.this.token
-  load_config_file       = false
 }
